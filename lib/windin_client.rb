1eb17f2076481893b5a97fdd7e1418ebaa372e3b
dyj <d41689@gmail.com>
2012-05-27 11:33:27 +0800

    move indicator_tools to lib directory

--- lib/windin_client.rb
+++ lib/windin_client.rb
@@ -13,8 +13,6 @@ class WindinClient
   def initialize()
     @array_data = []
     @current_day = nil
-    @code_stock_id_map = {}
-    puts 'in WindinClient #@code_stock_id_map'
   end
 
   def get_url(market, code)
@@ -161,8 +159,8 @@ class WindinClient
           unless (a_stock_financial_data.nil?)
             a_stock_financial_data.profit_ttm = x[1]
             a_stock_financial_data.mgjzc = x[2]
-            a_stock_financial_data.jzcsyl_ttm = x[3]
-            a_stock_financial_data.mll_ttm = x[4]
+            a_stock_financial_data.jzcsyl_ttm = x[-3]
+            a_stock_financial_data.mll_ttm = x[-2]
           end
         end
         stock_hash.each_value do |x|
@@ -255,10 +253,124 @@ class WindinClient
     end
     data
   end
+
+  def self.get_stock_financial_object(day, stock_id)
+    @@fin_data_map = {} unless @@fin_data_map
+    if @@fin_data_map.has_key? day
+      stock_fin_map = @@fin_data_map[day]
+      if stock_fin_map.has_key? stock_id
+        return stock_fin_map[stock_id]
+      else
+        return stock_fin_map[stock_id] = StockFinancialDatum.find_by_stock_id_and_day(stock_id, day)
+      end
+    else
+      stock_fin_map = {}
+      @@fin_data_map[day] = stock_fin_map
+      return stock_fin_map[stock_id] = StockFinancialDatum.find_by_stock_id_and_day(stock_id, day)
+    end
+  end
+
+  def self.prepare
+    puts 'in prepare....'
+    @@id_2_stock = {}
+    @@fin_data_map = {}
+    the_calculated_day = BlockTtm.order("id desc").first.day
+    puts "the_calculated_day:#{the_calculated_day},#{the_calculated_day.class}"
+    the_newest_financial_data = StockFinancialDatum.order("id desc").first
+    all_days = StockFinancialDatum.where(["stock_id = ? and day >= ?", the_newest_financial_data.stock_id, the_calculated_day]).all
+
+    StockCategory.all.each do |stock_category|
+      puts "stock_category:#{stock_category.id},#{stock_category.name}"
+      a_stocks_sets = stock_category.stock_sets
+      #处理某天的数据
+      all_days.each do |the_day|
+        puts "processing category:#{stock_category.id}, day:#{the_day.day}..."
+        a_block_ttm = BlockTtm.find_by_stock_category_id_and_day(stock_category.id, the_day.day)
+        next if a_block_ttm
+        total_capital = 0
+        total_profit = 0
+        total_jzc = 0
+        total_zysr = 0
+        total_zylr = 0
+        total_ttm = 0
+        total_ttm_counter = 0
+        total_mrq = 0
+        total_mrq_counter = 0
+        total_jzcsyl = 0
+        total_jzcsyl_counter = 0
+        total_mll = 0
+        total_mll_counter = 0
+
+        a_stocks_sets.each do |stock|
+          the_stock = nil
+          if @@id_2_stock.has_key? stock.stock_id
+            the_stock = @@id_2_stock[stock.stock_id]
+          else
+            the_stock = Stock.find(stock.stock_id)
+            @@id_2_stock[stock.stock_id] = the_stock
+          end
+
+          next if the_stock.nil?
+          next unless the_stock.is_stock?
+          stock_financial_data = self.get_stock_financial_object(the_day.day, stock.stock_id) #.order("id desc").first #.each do |stock_financial_data|
+          unless stock_financial_data.nil?
+            next if stock_financial_data.has_nil_field?
+            total_capital += stock_financial_data.zsz unless stock_financial_data.zsz.nil?
+            total_profit += stock_financial_data.profit_ttm * stock_financial_data.zgb
+            total_jzc += stock_financial_data.mgjzc * stock_financial_data.zgb
+            total_zysr += stock_financial_data.zysr_ttm unless stock_financial_data.zysr_ttm.nil?
+            total_zylr += stock_financial_data.zysr_ttm * stock_financial_data.mll_ttm unless stock_financial_data.zysr_ttm.nil? || stock_financial_data.mll_ttm.nil?
+            unless stock_financial_data.pe_ttm.nil?
+              total_ttm += stock_financial_data.pe_ttm
+              total_ttm_counter += 1
+            end
+            unless stock_financial_data.mrq.nil?
+              total_mrq += stock_financial_data.mrq
+              total_mrq_counter += 1
+            end
+            unless stock_financial_data.jzcsyl_ttm.nil?
+              total_jzcsyl += stock_financial_data.jzcsyl_ttm
+              total_jzcsyl_counter += 1
+            end
+
+            unless stock_financial_data.mll_ttm.nil?
+              total_mll += stock_financial_data.mll_ttm
+              total_mll_counter += 1
+            end
+          end
+        end
+        begin
+          if total_profit != 0
+            a_block_ttm = BlockTtm.new
+            a_block_ttm.day = the_day.day
+            a_block_ttm.stock_category_id = stock_category.id
+
+            a_block_ttm.ttm = total_capital / total_profit
+            a_block_ttm.sttm = total_ttm / total_ttm_counter
+            a_block_ttm.wmrq = total_capital / total_jzc
+            a_block_ttm.smrq = total_mrq / total_mrq_counter
+            a_block_ttm.sjzcsyl_ttm = total_jzcsyl / total_jzcsyl_counter
+            a_block_ttm.wjzcsyl_ttm = total_profit / total_jzc
+            a_block_ttm.wmll_ttm = total_zylr / total_zysr
+            a_block_ttm.smll_ttm = total_mll / total_mll_counter
+            a_block_ttm.save
+          end
+        rescue Exception => e
+          puts e.message
+        end
+      end
+    end
+    # TODO : delete those data which has already calculated.
+  end
 end
 
 #上证新兴指数
 class SZXXClient
+
+  def initialize
+    @code_stock_id_map = {}
+    puts "in WindinClient #@code_stock_id_map"
+  end
 
   def update_sample_stocks_to_db(stock_codes)
     the_stock_category = StockCategory.find_by_family_and_code(1, '000067')
@@ -596,6 +708,7 @@ class SZXXClient
 
   def get_stock_ids_from_codes(codes, code_to_name_hash)
     stock_ids = []
+    debugger
     codes.each do |code|
       if @code_stock_id_map.has_key?(code)
         stock_ids << @code_stock_id_map[code]
